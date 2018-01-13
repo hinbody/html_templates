d11f4f0c22331a1298f8d5f643b59085fc6ef0bc
Harley Inbody <hinbody@gmail.com>
2018-01-12 19:48:31 -0500

    adds a submit button to the form template

--- form_template.php
+++ form_template.php
@@ -1,3 +1,9 @@
 <form action="form_handling_page" method="post">
-
+  <div>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name">
+  </div>
+  <div class="button">
+    <button type="submit">Submit data</button>
+  </div>
 </form>
